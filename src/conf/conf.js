e9f91e1451eac74c70a193d83659c3de30fefbd1
namay10 <namaygupta101201@gmail.com>
2024-05-16 20:29:58 +0530

    Update conf.js

--- src/conf/conf.js
+++ src/conf/conf.js
@@ -2,8 +2,8 @@
 
 
 const conf = {
-    appWriteUrl : String(import.meta.env.VITE_APPWRITE_URL),
-    appWriteProjectId : String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
+    appWriteUrl : "https://cloud.appwrite.io/v1",
+    appWriteProjectId : import.meta.env.VITE_APPWRITE_PROJECT_ID,
     appWriteDatabaseId : String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
     appWriteCollectionId : String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
     appWriteBucketId : String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
